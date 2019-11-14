207501cd6b8ec8ba60304287b36e307397116e9f
Nikolaos Dymitriadis <nikolaos.dymitriadis@westwing.pl>
2019-11-14 23:03:49 +0100

    Term type hierarchy

---
+++ src/main/scala/com/ambienttea/terms/Term.scala
@@ -0,0 +1,44 @@
+package com.ambienttea.terms
+
+import scala.language.implicitConversions
+import scala.util.Random
+
+sealed trait Term
+
+case class Variable(name: Symbol) extends Term {
+  override def toString: String = s"${name.name.toLowerCase.capitalize}?"
+}
+
+case class Atom(value: Symbol) extends Term {
+  override def toString: String = s"`${value.name.toLowerCase}`"
+}
+
+case class Number(value: Int) extends Term {
+  override def toString: String = value.toString
+}
+
+case class Functor(op: Symbol, arguments: List[Term]) extends Term {
+  override def toString: String = s"$op(${arguments.map(_.toString).mkString(", ")})"
+}
+
+object Term {
+
+  def `_` = Variable(Symbol("_" + Random.nextString(6)))
+
+  implicit class SymbolOps(symbol: Symbol) {
+    def ? : Variable = Variable(symbol)
+
+    def apply(args: Term*): Functor = Functor(symbol, args.toList)
+
+    def a: Atom = Atom(symbol)
+  }
+
+  object Conversions {
+    implicit def number(n: Int): Number = Number(n)
+
+    implicit def atom(s: Symbol): Atom = Atom(s)
+  }
+
+}
+
+
